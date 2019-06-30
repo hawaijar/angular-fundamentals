c6d8eeb8189688f536b0368fdb20f86730ba2cfd
Hawaijar.js <hawaijar.js@gmail.com>
2019-06-30 15:30:55 +0530

    Second commit - added a component 😇

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { EventsAppComponent } from './events-app.component';
+import { EventsComponent } from './events/events.component';
+import { EventsListComponent } from './events/events-list/events-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EventsAppComponent
+    EventsAppComponent,
+    EventsComponent,
+    EventsListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [], // for services
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }

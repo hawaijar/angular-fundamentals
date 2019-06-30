a279e79f17c787dff7db6272a7732d92978eff23
Hawaijar.js <hawaijar.js@gmail.com>
2019-06-30 19:30:08 +0530

    Added icons for each UI framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { EventsAppComponent } from './events-app.component';
 import { EventsComponent } from './events/events.component';
 import { EventsListComponent } from './events/events-list/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsComponent,
     EventsListComponent,
+    EventThumbnailComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/events/events-list/events-list.component.ts
+++ src/app/events/events-list/events-list.component.ts
@@ -2,31 +2,45 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'events-list',
-  template: `
-    <div>
-      <h1>Upcoming Angular Events</h1>
-      <hr/>
-      <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price:  \${{event.price}}</div>
-        <div>
-          <span>Location: {{event.location.address}}</span>
-          <span>&nbsp;</span>
-          <span>{{event.location.city}}, {{event.location.country}}</span>
-      </div>
-    </div>
-  `
+  templateUrl: './events-list.component.html'
 })
 export class EventsListComponent implements OnInit {
-  event = {
+  event1 = {
+    icon: "/assets/images/angular_logo.png",
     id: 1,
     name: 'Angular Connect',
     date: '01/02/2019',
     time: '10:00 am',
     price: 599.90,
-    imageUrl: '/assets/images/angularconnect-shield.png',
+    imageUrl: '/assets/images/angular_logo.png',
+    location: {
+      address: '1067 DT',
+      city: 'London',
+      country: 'England'
+    }
+  };
+  event2 = {
+    icon: "/assets/images/react_logo.svg",
+    id: 1,
+    name: 'React Connect',
+    date: '11/02/2019',
+    time: '10:00 am',
+    price: 599.90,
+    imageUrl: '/assets/images/react_logo.svg',
+    location: {
+      address: '1067 DT',
+      city: 'London',
+      country: 'England'
+    }
+  };
+  event3 = {
+    icon: "/assets/images/vue_logo.png",
+    id: 1,
+    name: 'Vue Connect',
+    date: '15/02/2019',
+    time: '10:00 am',
+    price: 599.90,
+    imageUrl: '/assets/images/vue_logo.png',
     location: {
       address: '1067 DT',
       city: 'London',
